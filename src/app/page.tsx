9614f6f56de6bb782e934952649219aedf96e1ba
🌻 jaak 🌻 <jacques@mohara.co>
2024-09-01 15:03:08 +0000

    feat (checkout): splitting out the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
 // import Link from "next/link";
 
 // import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import { CheckoutProduct } from "./_components/product";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-row items-center justify-center">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <p className="text-small tracking-tight sm:text-[1rem]">
+            Just testing for now, but this is where the images will come.
+          </p>
+        </div>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="text-xl font-extrabold tracking-tight sm:text-[2rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <p className="text-lg text-center">
-            Get started by editing{" "}
-            <code className="bg-gray-800 p-2 rounded-md text-sm font-mono">
-              apps/checkout/src/app/page.tsx
-            </code>
-          </p>
           <CheckoutProduct />
         </div>
       </main>
