e78900321cdf617a1ad83ccd70e2558922c19a88
🌻 jaak 🌻 <jacques@mohara.co>
2024-09-19 06:23:02 +0000

    feat (checkout): updated the .gitignore permissions and added example of clerk callback

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -4,6 +4,32 @@ import { HydrateClient } from "~/trpc/server";
 
 export default async function Order() {
   const user = auth();
+  /**
+   * {
+  user: {
+    actor: undefined,
+    sessionClaims: {
+      exp: 1725971708,
+      iat: 1725971648,
+      iss: 'https://moving-lynx-47.clerk.accounts.dev',
+      nbf: 1725971638,
+      sid: 'sess_2lsbHtID1AjoVLjzC0fXwTlEwU4',
+      sub: 'user_2lZWBmmo7G6mP7yFSIfIaQAbTVS'
+    },
+    sessionId: 'sess_2lsbHtID1AjoVLjzC0fXwTlEwU4',
+    userId: 'user_2lZWBmmo7G6mP7yFSIfIaQAbTVS',
+    orgId: undefined,
+    orgRole: undefined,
+    orgSlug: undefined,
+    orgPermissions: undefined,
+    getToken: [AsyncFunction (anonymous)],
+    has: [Function (anonymous)],
+    debug: [Function (anonymous)],
+    protect: [Function (anonymous)],
+    redirectToSignIn: [Function: redirectToSignIn]
+  }
+}
+   */
   console.log({ user });
   return (
     <HydrateClient>
